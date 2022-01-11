eb2a86bc0440efc7a2e0c8fc09f6aad7190db399
Maxim Vainikka <maxim.vainikka@yandex.ru>
2022-01-11 21:39:10 +0300

    Initial Commit

---
+++ fetchingFriends/fetchingFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchingFriendsApp.swift
+//  fetchingFriends
+//
+//  Created by Максим Нуждин on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct fetchingFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
